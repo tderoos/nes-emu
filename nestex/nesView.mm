6ab0ce88b45eb6d9ea0b99706ed251d91cb1a1a3
Tommy de Roos <tderoos@hx2a.com>
2022-01-10 20:23:40 +0100

    Removed debug code

--- nestex/nesView.mm
+++ nestex/nesView.mm
@@ -85,12 +85,6 @@
 
     glBindTexture(GL_TEXTURE_2D, mTexture[mCurTexture]);
     glTexImage2D( GL_TEXTURE_2D, 0, GL_RGBA, 256, 256, 0, GL_BGRA, GL_UNSIGNED_INT_8_8_8_8_REV, inBuffer );
-    
-    GLenum error = glGetError();
-    if (error != GL_NO_ERROR)
-    {
-        NSLog(@"error");
-    }
 
 //    [self drawRect:[self bounds]];
     [self setNeedsDisplay:YES];
